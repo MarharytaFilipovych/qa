0c6345fc1099a8daa42dd466b3ba8edb992b0a65
Marharyta Filipovych <marharyta.filipovych@cantire.com>
2026-06-26 12:10:58 +0300

    context loads tests for motification and gateway services

---
+++ notification-service/src/test/java/com/microservices/margo/notification_service/NotificationServiceApplicationTest.java
@@ -0,0 +1,15 @@
+package com.microservices.margo.notification_service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@DisplayName("NotificationServiceApplication tests")
+class NotificationServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
